d8f545ef2306ae431011d1855a77094e56f3a984
Andre Cantarino <cantarino.andre@gmail.com>
2016-08-17 18:17:25 -0300

    pagina inicial

--- config/express.js
+++ config/express.js
@@ -1,14 +1,23 @@
 //config/express.js
-var express = require('express');
+var express = require('express'),
+	load = require('express-load');
 
 module.exports = function(){
 	var app = express();
-	
+
 	//configuracao de ambiente
 	app.set('port', 3000);
 
 	//middleware
 	app.use(express.static('./public'));
+	load('models', {cwd: 'app'})
+	.then('controllers')
+	.then('routes')
+	.into(app);
+
+	// views
+	app.set('view engine', 'ejs');
+	app.set('views','./app/views');
 
 	return app;
 }
